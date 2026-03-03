d0ad8486d96924fa2c6bdbebf769f52e742248b9
Anghievisck <pedro.luis.angh@gmail.com>
2026-03-03 20:08:07 -0300

    Aula 1 pt2

--- exemples/aula1.pl
+++ exemples/aula1.pl
@@ -1,3 +1,5 @@
+:- dynamic man/1.
+
 woman(ana).
 man(joao).
 man(paulo).
@@ -12,11 +14,36 @@ mom(X) :-
 
 /*
   How to run: swipl -s aula1 or swipl then [aula1].
-  Then you can ask based on the caracterics of the predicates, for example:
+  Then you can ask based on characteristics and relations, for example:
 
-  ?- mom(ana). 
-  true
+  ?- mom(ana).
+  true.
 
   ?- mom(paulo).
-  false 
+  false.
+
+  ?- child(paulo, Mom).
+  Mom = ana.
+
+
+  The ":- dynamic <smth>/n." allows to add facts to 'smth' (it can be a characteristic or a relation) during the execution
+  also, the n means the number of parameters 'smth' has.
+
+  To add during execution, use the command assertz() or asserta().
+  assertz(smth(x1, ..., xn)) add (x1, ..., xn) to the end of 'smth' list of facts.
+  asserta(smth(x1, ..., xn)) add (x1, ..., xn) to the start of 'smth' list of facts.
+
+  When a relation/characteristic has multiple answers, you can type ';' after a answer to get the next one, for example:
+
+  ?- man(Man).
+  Man = joao ;
+  Man = paulo.
+
+  ?- assertz(man(pedro)).
+  ?- asserta(man(kaina)).
+  ?- man(Man).
+  Man = kaina ;
+  Man = joao ;
+  Man = paulo ;
+  Man = pedro.
 */
